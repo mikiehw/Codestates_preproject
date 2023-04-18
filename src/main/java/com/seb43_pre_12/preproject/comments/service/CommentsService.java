dafc64aeb1c3d1e6162118398ecabb07b5f6b4f5
hyunwoojo <hwchokim95@hanmail.net>
2023-04-18 16:13:41 +0900

    CRUD 일부 수정

---
+++ src/main/java/com/seb43_pre_12/preproject/comments/service/CommentsService.java
@@ -0,0 +1,67 @@
+package com.seb43_pre_12.preproject.comments.service;
+
+import com.seb43_pre_12.preproject.comments.entity.Comments;
+import com.seb43_pre_12.preproject.comments.repository.CommentsRepository;
+
+import com.seb43_pre_12.preproject.exception.BusinessLogicException;
+import com.seb43_pre_12.preproject.exception.ExceptionCode;
+import com.seb43_pre_12.preproject.member.service.MemberService;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Optional;
+
+
+
+@Service
+@Transactional
+public class CommentsService {
+
+    private final CommentsRepository commentsRepository;
+    private final MemberService memberService;
+
+    public CommentsService(CommentsRepository commentsRepository, MemberService memberService) {
+        this.commentsRepository = commentsRepository;
+        this.memberService = memberService;
+    }
+
+    public Comments createComment(Comments comment) {
+        return commentsRepository.save(comment);
+    }
+
+    public Comments updateComment(Comments comment) {
+        Comments findComment = findVerifiedComment(comment.getCommentId());
+
+        Optional.ofNullable(comment.getComment())
+                .ifPresent(text -> findComment.setComment(text));
+        findComment.setModifiedAt(LocalDateTime.now());
+
+        return commentsRepository.save(findComment);
+    }
+    
+
+    public Comments findComment(long commentId) {
+        return commentsRepository.findById(commentId).get();
+    }
+
+    public List<Comments> findComments() {
+        return commentsRepository.findAll();
+    }
+
+
+
+    public void deleteComment(long commentId) {
+        commentsRepository.deleteById(commentId);
+    }
+
+    public Comments findVerifiedComment(long commentId) {
+        Optional<Comments> optionalComments = commentsRepository.findById(commentId);
+
+        Comments findComment =
+                optionalComments.orElseThrow(() ->
+                        new BusinessLogicException(ExceptionCode.COMMENT_NOT_FOUND));
+        return findComment;
+    }
+
+}
---
+++ src/main/java/com/seb43_pre_12/preproject/member/entity/Member.java
@@ -0,0 +1,75 @@
+package com.seb43_pre_12.preproject.member.entity;
+
+
+import com.seb43_pre_12.preproject.answers.entity.Answer;
+import com.seb43_pre_12.preproject.comments.entity.Comments;
+import com.seb43_pre_12.preproject.question.entity.Question;
+
+import lombok.*;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+@Getter
+@Setter
+@Entity
+@NoArgsConstructor
+public class Member {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long memberId;
+
+
+    @Column(nullable = false, updatable = false, unique = true)
+
+    private String email;
+
+    @Column(length = 100, nullable = false, unique = true)
+    private String username;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Enumerated(EnumType.STRING)
+
+    @Column(length = 20, nullable = false, name = "MEMBER_STATUS")
+    private MemberStatus memberStatus = MemberStatus.MEMBER_ACTIVE;
+
+
+    @Column(nullable = false)
+    private LocalDateTime createdAt = LocalDateTime.now();
+
+    @Column(nullable = false, name = "LAST_MODIFIED_AT")
+    private LocalDateTime modifiedAt = LocalDateTime.now();
+
+
+
+    // N : 1(Member) 양방향 매핑
+    @OneToMany(mappedBy = "member")
+    private List<Comments> comments = new ArrayList<>();
+
+
+    @OneToMany(mappedBy = "member")
+    private List<Answer> answers = new ArrayList<>();
+
+    @OneToMany(mappedBy = "member")
+    private List<Question> questions = new ArrayList<>();
+
+
+    public enum MemberStatus {
+        MEMBER_ACTIVE("활동중"),
+        MEMBER_SLEEP("휴면 상태"),
+        MEMBER_QUIT("탈퇴 상태");
+
+        @Getter
+        private String status;
+
+        MemberStatus(String status) {
+            this.status = status;
+        }
+    }
+}
+
